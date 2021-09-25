4ca8a98463ea2f11245717da044d6143fd9c7e7d
[2기-2반] 이금홍 <guemhong3780@gmail.com>
2021-09-25 23:40:54 +0900

    BJ_1406_f

--- 백준 2cycle/1406.py
+++ 백준 2cycle/1406.py
@@ -1,37 +1,30 @@
-# 다른 사람의 풀이
 import sys
-input = sys.stdin.readline
- 
-editor = ['L', 'D', 'B', 'P']
 
-word = input().rstrip()
-commands = []
-cusor = len(word) + 1
+# 입력부
+n = sys.stdin.readline().rstrip()
+b = int(sys.stdin.readline())
 
-for i in range(int(input())):
-    commands.append(input().replace('\n',''))
+# l_stack : 왼쪽 스택(문자열로 초기화)
+# r_stack : 오른쪽 스택
+l_stack = list(n)
+r_stack = []
 
-for i in commands:
-    if 'P' in i:
-        if cusor >= len(word):
-            word = word[:cusor] + i[2]
-        else:
-            word = word[:cusor] + i[2] + word[-1]
-    elif i == 'L':
-        if cusor < 1:
-            pass
-        else:
-            cusor -= 1
-    elif i == 'D':
-        if cusor == len(word) + 1:
-            pass
-        else:
-            cusor += 1
+for s in range(b):
+    # x : 쿼리
+    x = sys.stdin.readline().rstrip()
+    # 만일 왼쪽으로 이동 시 왼쪽에서 오른쪽으로 옮김
+    if x == "L" and l_stack:
+        r_stack.append(l_stack.pop())
+    # 만일 오른쪽으로 이동 시 오른쪽에서 왼쪽으로 옮김
+    elif x == "D" and r_stack:
+        l_stack.append(r_stack.pop())
+    # 만일 삭제 시 왼쪽에서 하나 삭제
+    elif x == "B" and l_stack:
+        l_stack.pop()
+    # 만일 삽입 시 왼쪽에 문자 삽입
     else:
-        if cusor < 1:
-            pass
-        else:
-            cusor -= 1
-            word = word[:cusor]
+        if x[0] == "P":
+            l_stack.append(x[2])
 
-print(word)
+# 오른쪽 스택을 뒤집어서 왼쪽 스택과 합친 후 출력
+print(''.join(l_stack + r_stack[::-1]))
