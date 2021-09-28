9dd1b4eae199de695bf69b606c5ebf3af49a2709
[2기-2반] 이금홍 <guemhong3780@gmail.com>
2021-09-28 14:47:18 +0900

    bj_10820_f

---
+++ 백준 2cycle/10820.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    line = input().rstrip('\n')
+
+    if not line:
+        break
+
+    # 소문자, 대문자, 숫자, 공백
+    l, u, d, s = 0, 0, 0, 0
+    for each in line:
+        if each.islower():
+            l += 1
+        elif each.isupper():
+            u += 1
+        elif each.isdigit():
+            d += 1
+        elif each.isspace():
+            s += 1
+
+    print(l, u, d, s)
