7a44dcdc30c1fe686725942e04dd4153fb6f85a7
[2기-2반] 이금홍 <guemhong3780@gmail.com>
2021-11-01 15:57:25 +0900

    11052 수정, bj_16194_f

--- 백준 2cycle/16194.py
+++ 백준 2cycle/16194.py
@@ -3,8 +3,8 @@ input = sys.stdin.readline
 
 N = int(input())
 dp = [0]+list(map(int,input().split()))
-#합이 N인것의 조합, dp[i]에 최댓값 갱신
+#합이 N인것의 조합, dp[i]에 최솟값 갱신
 for i in range(N+1):
     for j in range(1,i+1):
-        dp[i] = max(dp[i],dp[i-j]+dp[j])
+        dp[i] = min(dp[i],dp[i-j]+dp[j])
 print(dp[N])
