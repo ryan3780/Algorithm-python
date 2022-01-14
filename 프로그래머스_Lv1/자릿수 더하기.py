890c5a1f711035dacf51c70c6ee54760bcdfae43
[2기-2반] 이금홍 <guemhong3780@gmail.com>
2022-01-14 23:57:53 +0900

    자릿수 더하기 파이써닉?

--- 프로그래머스_Lv1/자릿수 더하기.py
+++ 프로그래머스_Lv1/자릿수 더하기.py
@@ -1,10 +1,7 @@
 def solution(n):
     answer = 0
     str_n = str(n)
-    str_n_list = []
-    
-    for i in str_n:
-        str_n_list.append(int(i))
+    str_n_list = [int(i) for i in str_n]
     
     answer = sum(str_n_list)
     
