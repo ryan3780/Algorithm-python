01f0580007c7647507db3d3b959dffe45820aeab
[2기-2반] 이금홍 <guemhong3780@gmail.com>
2022-01-22 23:54:45 +0900

    기능개발_f

---
+++ 프로그래머스_Lv2/기능개발.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def solution(progresses, speeds):
+    answer = []
+    progresses = deque(progresses)
+    speeds = deque(speeds)
+
+    count = 0
+    time = 0
+    while progresses:
+        if progresses[0] + speeds[0] * time >= 100:
+            count += 1
+            progresses.popleft()
+            speeds.popleft()
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+
+            time += 1
+
+    answer.append(count)
+    return answer
