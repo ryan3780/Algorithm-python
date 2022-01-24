abce925affd4e09bd84aba40dd6e1dfc39ba2e8c
[2기-2반] 이금홍 <guemhong3780@gmail.com>
2022-01-25 00:31:34 +0900

    타겟넘버_f

--- 프로그래머스_Lv2/타겟넘버.py
+++ 프로그래머스_Lv2/타겟넘버.py
@@ -5,4 +5,17 @@ def solution(numbers, target):
     q = deque()
     n = len(numbers)
     q.append([numbers[0], 0])
-    
+    q.append([-1*numbers[0], 0])
+
+    while q:
+        tmp, idx = q.popleft()
+        idx += 1
+        if idx < n:
+            q.append([tmp + numbers[idx], idx])
+            q.append([tmp - numbers[idx], idx])
+        else:
+            if tmp == target:
+                answer += 1
+
+    return answer
+
